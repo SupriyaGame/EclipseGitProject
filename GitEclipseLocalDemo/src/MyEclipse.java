0afc7a70941c6a984a571428afab73a9b60e5a69
SupriyaGame <supriyagame2011@gmail.com>
2019-03-23 15:32:40 +0530

    old branch

--- GitEclipseLocalDemo/src/MyEclipse.java
+++ GitEclipseLocalDemo/src/MyEclipse.java
@@ -4,7 +4,7 @@ public class MyEclipse {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		System.out.println("my first commit through eclipse");
-		System.out.println("my second commit");
+	
 	}
 
 }
